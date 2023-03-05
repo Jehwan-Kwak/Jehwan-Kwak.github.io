e49ec8e8f93d5b219e5a3f732f41e5e2cd027b34
Jehwan-Kwak <jehwane@naver.com>
2023-03-05 20:53:52 +0900

    challange

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+
+const API_KEY ="25032b234ca8e23804b4e9af5ed9afcb";
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = `in ${data.name}`;
+            weather.innerText = `${Math.round(data.main.temp)} degrees Celsius, ${data.weather[0].main}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
